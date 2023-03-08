21ebf067bb1d0b524a8fff1d1f96de00fd5de2de
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2023-03-08 00:50:52 -0400

    finish first challenge

---
+++ conhecendo_a_plataforma_2407/Main.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = sc.nextInt();
+        int j = sc.nextInt();
+
+        int interval[] = IntStream.rangeClosed(i, j).toArray();
+
+        int biggerSequenceSize = 0;
+
+        for (int num : interval) {
+            int size = getSequenceSize(num);
+            if (biggerSequenceSize < size) {
+                biggerSequenceSize = size;
+            }
+        }
+
+        System.out.println(String.format("%d %d %d", i, j, biggerSequenceSize));
+    }
+
+    public static int getSequenceSize(int number) {
+        int aux = number;
+        ArrayList<Integer> sequence = new ArrayList<Integer>();
+        while (aux != 1) {
+            if (aux % 2 == 0) {
+                aux = aux / 2;
+            } else {
+                aux = (aux * 3) + 1;
+            }
+            sequence.add(aux);
+        }
+        // System.out.println(sequence.toString() + " " + sequence.size());
+        return sequence.size();
+    }
+
+}
