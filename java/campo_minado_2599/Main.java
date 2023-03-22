ab1a282ccc8e9b58fed76524b41704a8c48b3e9d
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2023-03-22 02:05:12 -0400

    wip

--- java/campo_minado_2599/Main.java
+++ java/campo_minado_2599/Main.java
@@ -6,36 +6,102 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNextLine()) {
-            int m = sc.nextInt();
-            int n = sc.nextInt();
+            int rows = sc.nextInt();
+            int columns = sc.nextInt();
             sc.nextLine();
 
-            if (m == 0 && n == 0)
+            if (rows == 0 && columns == 0)
                 break;
 
-            char minefield[][] = new char[m][n];
+            MineField minefield = new MineField(rows, columns, sc);
 
-            System.out.println(m + " " + n);
-
-            populateMatrix(minefield, sc);
-            printMatrix(minefield);
+            minefield.print();
 
         }
 
     }
 
-    public static char[][] populateMatrix(char[][] minefield, Scanner sc) {
+}
+
+class MineField {
+    private int rowCount;
+    private int collCount;
+    private char[][] minefield;
+    private Scanner sc;
+
+    public MineField(int rowCount, int collCount, Scanner sc) {
+        this.rowCount = rowCount;
+        this.collCount = collCount;
+        this.sc = sc;
+
+        this.minefield = new char[rowCount][collCount];
+
+        this.populateMine();
+        this.verifyCells();
+    }
+
+    private void populateMine() {
         for (int line = 0; line < minefield.length; line++) {
-            char[] lineInput = sc.nextLine().toCharArray();
-            minefield[line] = lineInput;
+            char[] lineInput = this.sc.nextLine().toCharArray();
+            this.minefield[line] = lineInput;
         }
-        return minefield;
     }
 
-    public static void printMatrix(char[][] minefield) {
+    private void verifyCells() {
+        for (int line = 0; line < minefield.length; line++) {
+            for (int col = 0; col < minefield[line].length; col++) {
+                this.minefield[line][col] = this.countMinesInAdjacencies(line, col);
+            }
+        }
+    }
+
+    public void print() {
 
         for (int line = 0; line < minefield.length; line++) {
             System.out.println(minefield[line]);
         }
     }
+
+    private char countMinesInAdjacencies(int row, int col) {
+        int minesInAdjacencies = 0;
+
+        if (validateCell(row - 1, col)) // encima
+            minesInAdjacencies++;
+        if (validateCell(row, col - 1)) // esquerda
+            minesInAdjacencies++;
+        if (validateCell(row + 1, col)) // embaixo
+            minesInAdjacencies++;
+        if (validateCell(row, col + 1))// direita
+            minesInAdjacencies++;
+        if (validateCell(row - 1, col + 1)) // canto direito inferior
+            minesInAdjacencies++;
+        if (validateCell(row - 1, col - 1)) // canto esquerdo inferior
+            minesInAdjacencies++;
+        if (validateCell(row + 1, col + 1)) // canto direito superior
+            minesInAdjacencies++;
+        if (validateCell(row + 1, col - 1)) // canto esquerdo superior
+            minesInAdjacencies++;
+
+        return (char)(minesInAdjacencies + '0');
+    }
+
+    public boolean validateCell(int row, int col) {
+        if ((row <= this.rowCount && row > 0) && (col <= this.collCount && col > 0)) {
+
+            return validateMine(row, col);
+
+        } else {
+            return false;
+        }
+
+    }
+
+    private boolean validateMine(int row, int col) {
+        if (this.minefield[row][col] == '*') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
