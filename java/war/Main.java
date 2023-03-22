3411b4b841b742882a1c722995faf35cab4fbc0a
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2023-03-21 20:13:32 -0400

    wip

--- java/war/Main.java
+++ java/war/Main.java
@@ -6,25 +6,41 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (sc.hasNext()) {
-            ArrayList<String> playerA = getLineCards(sc);
-            ArrayList<String> playerB = getLineCards(sc);
+            ArrayList<Integer> playerA = getLineCards(sc);
+            ArrayList<Integer> playerB = getLineCards(sc);
 
             System.out.println(playerA);
             System.out.println(playerB);
 
+            ArrayList<Integer> table = new ArrayList<Integer>();
             while (!playerA.isEmpty() || !playerB.isEmpty()) {
-                
+                // Player A Win
+                if (playerA.get(0) > playerB.get(0)) {
+
+                    // Player B Win
+                } else if (playerA.get(0) > playerB.get(0)) {
+
+                    // Equal
+                } else {
+
+                }
             }
         }
 
     }
 
-    public static ArrayList<String> getLineCards(Scanner sc) {
+    // Converter para integer
+    public static ArrayList<Integer> getLineCards(Scanner sc) {
         ArrayList<String> inputs = new ArrayList<String>(Arrays.asList(sc.nextLine().split(" ")));
+
+        ArrayList<Integer> cards = new ArrayList<Integer>();
 
         inputs.replaceAll(e -> e.replaceAll("[^0-9]", ""));
 
-        return inputs;
+        for (String card : inputs) {
+            cards.add(Integer.parseInt(card));
+        }
+
+        return cards;
     }
 }
-
