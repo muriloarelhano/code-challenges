47fdbede2a97d4da1673939c46cccc6ade606c69
Murilo Esteca Arelhano <51941771+muriloarelhano@users.noreply.github.com>
2023-06-30 21:14:17 -0400

    só deus

--- caixeiro_2721/main.py
+++ caixeiro_2721/main.py
@@ -1,36 +1,30 @@
-def dfs(graph, start, visited):
-    visited[start] = True
+def backtrack(graph, visited, path, start, count):
+
+    if count == len(graph) - 1 and start == path[0]:
+        return True  # Hamiltonian cycle found
     for neighbor in graph[start]:
         if not visited[neighbor]:
-            dfs(graph, neighbor, visited)
+            visited[neighbor] = True
+            path.append(neighbor)
+            if backtrack(graph, visited, path, neighbor, count+1):
+                return True  # Hamiltonian cycle found
+            visited[neighbor] = False
+            path.pop()
+    return False
 
 
-def is_possible(num_cities, num_roads, origin, roads):
-    graph = [[] for _ in range(num_cities + 1)]
-    for road in roads:
-        city1, city2 = road
-        graph[city1].append(city2)
-        graph[city2].append(city1)
-
-    visited = [False] * (num_cities + 1)
-    dfs(graph, origin, visited)
-
-    for i in range(1, num_cities + 1):
-        if not visited[i]:
-            return "NAO EH POSSIVEL"
-
-    return "EH POSSIVEL"
-
-
-num_test_cases = int(input())
-
-for _ in range(num_test_cases):
-
-    num_cities, num_roads, origin = map(int, input().split())
-    roads = []
-    for _ in range(num_roads):
-        road = tuple(map(int, input().split()))
-        roads.append(road)
-
-    result = is_possible(num_cities, num_roads, origin, roads)
-    print(result)
+t = int(input())  # number of test cases
+for _ in range(t):
+    j, k, l = map(int, input().split())
+    graph = [[] for _ in range(j+1)]
+    for _ in range(k):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+    visited = [False] * (j+1)
+    visited[l] = True
+    path = [l]
+    if backtrack(graph, visited, path, l, 0):
+        print("EH POSSIVEL")
+    else:
+        print("NAO EH POSSIVEL")
